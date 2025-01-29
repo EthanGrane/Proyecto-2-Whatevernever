7d58d3f4177472955671823af8f4c38835dc0439
nilbalaguer <nilbalaguerfp@ibf.cat>
2025-01-29 17:55:50 +0100

    HtmlCss login i perfil basico

--- Proyecto2Base/resources/js/views/PerfilView.vue
+++ Proyecto2Base/resources/js/views/PerfilView.vue
@@ -2,5 +2,17 @@
 </script>
 
 <template>
-    <p>Perfil</p>
+    <div id="fondoperfil">
+        <div>
+            <img src="../../../public/images/mono.webp" alt="Pablo">
+            <div id="informacionperfil">
+                <h2>Usuario 1</h2>
+                <p>Informacion Personal</p>
+            </div>
+        </div>
+        <div id="map">
+        </div>
+
+        <button id="botonmarcadoresperfil">></button>
+    </div>
 </template>
--- Proyecto2Base/resources/js/views/auth/LoginView.vue
+++ Proyecto2Base/resources/js/views/auth/LoginView.vue
@@ -2,5 +2,23 @@
 </script>
 
 <template>
-    <p>Login</p>
+    <div id="fondoiniciosession">
+        <div>
+            <h1>Whatevernever</h1>
+        </div>
+        <div>
+            <h2>Inicio de Session</h2>
+            <form>
+                <label>Email</label>
+                <br>
+                <input>
+                <br><br>
+                <label>Contraseña</label>
+                <br>
+                <input>
+                <br><br>
+                <input type="submit" value="Acceso" class="boton2">
+            </form>
+        </div>
+    </div>
 </template>
