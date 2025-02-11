e4837ddb1fb304a9e92a6810d171a7cc0aba04c3
nilbalaguer <nilbalaguerfp@ibf.cat>
2025-02-11 18:34:59 +0100

    search view

--- BaseProyectoFinal/resources/js/views/search/searchView.vue
+++ BaseProyectoFinal/resources/js/views/search/searchView.vue
@@ -1,6 +1,35 @@
 <template>
-    <h1>Search page</h1>
+    <div id="fondobuscador">
+        <div>
+            <input :placeholder="$t('buscadoramigos')">
+        </div>
+        <div id="resultadobusqueda">
+            <div v-for="(user, index) in users" :key="index" class="searchuserdiv">
+                <div class="infousersearch">
+                    <div>
+                        <img :src="user.image" :alt="user.name">
+                    </div>
+                    <div>
+                        <b><p>{{ user.name }}</p></b>
+                        <p>{{ user.username }}</p>
+                    </div>
+                </div>
+                <div>
+                    <button class="ternaryButton">{{ $t('addfriendbutton') }}</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+//Se deveria obtener de la base de datos con la api
+const users = ref([
+    { name: "Rana Gustavo", username: "ranagustavo", image: "/images/users/ranagustavo.webp" },
+    { name: "Juan Pérez", username: "juanperez", image: "/images/users/ranagustavo.webp" },
+    { name: "Maria López", username: "marialopez", image: "/images/users/ranagustavo.webp" }
+]);
+
 </script>
