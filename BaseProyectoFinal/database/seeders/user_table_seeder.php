23b1d0d55f3d05c8c51c13c9734648296313b6e4
Ethan Grané G <90970199+EthanGrane@users.noreply.github.com>
2025-02-18 15:51:12 +0100

    Admin user on Seeder

--- BaseProyectoFinal/database/seeders/user_table_seeder.php
+++ BaseProyectoFinal/database/seeders/user_table_seeder.php
@@ -17,6 +17,15 @@ class user_table_seeder extends Seeder
     {
         $faker = Faker::create();
 
+        DB::table('users')->insert([
+            'username' => "admin",
+            'name' => "AdminName",
+            'email' => "Admin@Admin.com",
+            'password' => Hash::make('password'),
+            'last_lng' => mt_rand(-9000,3000) / 1000,
+            'last_lat' => mt_rand(37000,43000) / 1000,
+        ]);
+
         foreach (range(1, 50) as $index) 
         {
             DB::table('users')->insert([
