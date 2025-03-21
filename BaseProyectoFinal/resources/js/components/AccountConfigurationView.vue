78e8084a4a4abe68f44de498ea7244c6b6716d2e
nilbalaguer <nilbalaguerfp@ibf.cat>
2025-03-21 20:06:34 +0100

    Pagina configuracion i logout

---
+++ BaseProyectoFinal/resources/js/components/AccountConfigurationView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <h2>Configuracion cuenta</h2>
+    <hr>
+    <h2>El Alejandro</h2>
+    <p>Tiene el frenillo como una goma de pollo</p>
+    <hr>
+    <h2>A tu madre</h2>
+    <p>Le cuelgan las pechugas</p>
+    <hr>
+</template>
--- BaseProyectoFinal/resources/js/layouts/Header.vue
+++ BaseProyectoFinal/resources/js/layouts/Header.vue
@@ -2,18 +2,17 @@
     <header>
         <div>
             <router-link to="/friends">
-                <img src="/images/icon_notification-bell.svg" class="icon icon-shake-hover">
+                <img src="/images/icon_notification-bell.svg" class="icon icon-shake-hover" alt="Solicitudes de amistad">
             </router-link>
         </div>
         <div>
             <h2 class="brandName">{{$t('brandName')}}.</h2>
         </div>
         <div>
-            <LocaleSwitcher />
+            <img src="/images/emoji_pinRed.png" class="icon icon-shake-hover" alt="Pin para marcar el mapa">
         </div>
     </header>
 </template>
 
 <script setup>
-import LocaleSwitcher from "../components/LocaleSwitcher.vue";
 </script>
