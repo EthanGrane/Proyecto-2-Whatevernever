0386b46ecb8296cdfad4dcdf0b6c0cbf84057b7d
Ethan Grané G <90970199+EthanGrane@users.noreply.github.com>
2025-04-04 19:32:12 +0200

    PrimeVue Popover configured

--- BaseProyectoFinal/resources/js/app.js
+++ BaseProyectoFinal/resources/js/app.js
@@ -58,6 +58,7 @@ import InputIcon from 'primevue/inputicon';
 import MeterGroup from 'primevue/metergroup';
 import Tooltip from 'primevue/tooltip';
 import FloatLabel from 'primevue/floatlabel';
+import Popover from 'primevue/popover'
 
 /*STYLES */
 /*
@@ -161,6 +162,7 @@ app.component('Editor', Editor);
 app.component('IconField', IconField);
 app.component('InputIcon', InputIcon);
 app.component('MeterGroup', MeterGroup);
+app.component('Popover', Popover)
 
 app.directive('tooltip', Tooltip);
 app.directive('ripple', Ripple);
